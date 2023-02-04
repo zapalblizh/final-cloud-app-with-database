febf781a4cfcc1fca1135e99079d62830ff321d3
zapalblizh <zapal.blizh@gmail.com>
2023-02-04 03:09:49 -0500

    Starting Lab 2

--- myproject/settings.py
+++ myproject/settings.py
@@ -80,6 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'PASSWORD': 'MTU4MDYtemFwYWxi',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
